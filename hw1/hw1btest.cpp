adea6e3b37278f225d749685c2ad0ebec898ead1
Nicholas DiMaria <ndsoccerstar55@gmail.com>
2019-01-27 12:50:31 -0500

    Fix Hw1b

--- hw1/hw1btest.cpp
+++ hw1/hw1btest.cpp
@@ -28,14 +28,14 @@ double choose(int n, int r){
 int main() {
 	int testCases[][3] = {
 		{0, 0, 1},
-		
+
 		{1, 0, 1},
 		{1, 1, 1},
 
 		{2, 0, 1},
 		{2, 1, 2},
 		{2, 2, 1},
-		
+
 		{5, 0, 1},
 		{5, 1, 5},
 		{5, 2, 10},
@@ -43,12 +43,12 @@ int main() {
 		{6, 3, 20}
 	};
 	const int numTestCases = 10;
-	
+
 	for (int i = 0; i < numTestCases; i++) {
 		int n = testCases[i][0];
 		int r = testCases[i][1];
 		int result = testCases[i][2];
-		
+
 		if (choose(n, r) != result) {
 			cerr << "Failed test case: choose(" << n << ", " << r << ") = " << result << '\n';
 		}
